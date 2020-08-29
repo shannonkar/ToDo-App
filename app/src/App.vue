80fadb1ad5a7848e6a760ecb5dcd611cf9e44b44
Shannon Makenna <shannonmakenna@chartyn.com>
2020-08-29 12:11:33 -0400

    create vue app

---
+++ app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+
+    <ToDo />
+  </div>
+</template>
+
+<script>
+import ToDo from './components/ToDo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ToDo
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
